c868f3577a840dc911e4aace0390ecaec125531e
S444-gold <goldoyinloluwa@gmail,com>
2024-07-13 06:47:41 -0400

    Finishing touches

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::env;
 use sha2::{Sha256, Digest};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::error::Error;
+use std::process;
 
 struct Args{
     wanted_hash: String,
@@ -21,6 +23,40 @@ impl Args {
     }
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+fn crack(crack_args: Args) -> Result<(), Box<dyn Error>>{
+    let password_list = File::open(crack_args.password_file)?;
+    let reader = BufReader::new(password_list);
+
+    let mut attempts = 1;
+
+    for line in reader.lines(){
+        let line = line.unwrap();
+        let password = line.trim().to_owned().into_bytes();
+        let password_hash = format!("{:x}", Sha256::digest(&password));
+        //converts password into sha256 hash
+        println!("|{}| {:?} {}",attempts, std::str::from_utf8(&password).unwrap(), password_hash);
+
+        if crack_args.wanted_hash ==  password_hash{
+            println!("|{}| Password Matched\nPassword: {:?}\nHash: {}",attempts, std::str::from_utf8(&password).unwrap(), password_hash);
+            process::exit(0);
+        }
+        attempts += 1;
+    }
+    Ok(println!("Password Not Found"))
 }
+fn main() {
+    let config_args: Vec<String> = env::args().collect();
+    let args = Args::build(&config_args).unwrap_or_else(|err|{
+        eprintln!("Error Running Application: {}", err);
+        process::exit(0);
+    });
+
+    if let Err(e) = crack(args){
+        eprintln!("Error Cracking Password: {}", e);
+        process::exit(0); 
+    }
+}
+
+
+
+
